2094094f8856254bcbcba0eec6edde5dc37e12e1
Ilse-hutten <huttenilse@gmail.com>
2025-03-19 14:53:59 +0000

    Force push updates to app.py

--- gcp_env.sh
+++
@@ -1,4 +0,0 @@
-export GCP_PROJECT_ID="lewagon-statistical-arbitrage"
-export DOCKER_IMAGE_NAME="stat-api-interface"
-export GCP_REGION="europe-west1"
-export DOCKER_REPO_NAME="stat-replica-lewagon"
